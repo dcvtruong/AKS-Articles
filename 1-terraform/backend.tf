5279d3c1cec8b47acf46cff6502a919a1e269544
dcvtruong <45860078+dcvtruong@users.noreply.github.com>
2020-03-02 14:45:10 -0600

    Update backend.tf

--- 1-terraform/backend.tf
+++ 1-terraform/backend.tf
@@ -1,5 +1,9 @@
 terraform {
   backend "azurerm" {
+    subscription_id      = "ae1cfcb2-2928-405d-afd3-743efe4dddd8"
+    resource_group_name  = "AksTerraform-Sandbox-ITY-CC-Backend-RG"
+    storage_account_name = "sandboxsaterraformstate"
+    container_name       = "sandboxitycctfstate"
     key                  = "demo.terraform.tfstate"
   }
 }
